521e98c63d5b0fcf3f0727f6ba7d1a3cc6e3b49a
AyushSinha <kumar.a7ush9@gmail.com>
2025-08-03 11:56:37 +0530

    downloading the requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="bot-M",
+    version="0.1.0",
+    author="AyushSinha- @DSwithBappy",
+    author_email="kumar.a7ush9@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
